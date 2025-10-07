2f3787577f70765cb23b4f669a5dea60294053cc
AndresTorres05 <pipe180503@gmail.com>
2025-10-07 10:30:07 -0500

    Update AdicionalController.java

--- src/main/java/com/example/demo/controller/AdicionalController.java
+++ src/main/java/com/example/demo/controller/AdicionalController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "[http://localhost:4200](http://localhost:4200)")
-@RestController
-@RequestMapping("/api/adicionales")
+//@CrossOrigin(origins = "[http://localhost:4200](http://localhost:4200)")
+//@RestController
+//@RequestMapping("/api/adicionales")
 public class AdicionalController {
 
     @Autowired
